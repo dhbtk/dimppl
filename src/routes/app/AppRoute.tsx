c27c111bfe74c09998889af7f3c4197482344e48
Diana Horbatiuk <diana@dianahorbatiuk.com>
2024-04-06 19:32:28 -0300

    barebones dark mode

--- src/routes/app/AppRoute.tsx
+++ src/routes/app/AppRoute.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 import { Outlet } from '@tanstack/react-router'
 import { Sidebar } from './Sidebar.tsx'
 import { PlayerControlsTopBar } from './PlayerControlsTopBar.tsx'
+import styled from 'styled-components'
+
+const RootDiv = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  border-top-right-radius: 9px;
+  border-bottom-right-radius: 9px;
+  @media (prefers-color-scheme: dark) {
+    background: transparent;
+  }
+  @media (prefers-color-scheme: light) {
+    background: #FFF;
+  }
+`
 
 export const AppRoute: React.FC = () => {
   return (
     <div style={{ display: 'flex' }}>
       <Sidebar/>
-      <div style={{ flex: '1', display: 'flex', flexDirection: 'column', maxHeight: '100vh', background: '#fff', borderTopRightRadius: 9, borderBottomRightRadius: 9 }}>
+      <RootDiv>
         <PlayerControlsTopBar/>
         <Outlet/>
-      </div>
+      </RootDiv>
     </div>
   )
 }
